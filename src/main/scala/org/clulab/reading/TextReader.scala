dd4579f4717efd1d4caa8edd9ffa7bf012fa48ee
bsharpataz <bsharp@email.arizona.edu>
2020-04-26 08:54:11 -0700

    text reader

--- src/main/scala/org/clulab/reading/TextReader.scala
+++ src/main/scala/org/clulab/reading/TextReader.scala
@@ -1,13 +1,44 @@
 package org.clulab.reading
 
-import ai.lum.odinson.{Document => OdinsonDocument}
+import java.io.File
+
+import ai.lum.common.ConfigUtils._
+import ai.lum.common.FileUtils._
+import ai.lum.common.TryWithResources._
+import ai.lum.common.ConfigFactory
+import ai.lum.odinson.extra.ProcessorsUtils
+import ai.lum.odinson.{ExtractorEngine, Document => OdinsonDocument}
 import org.clulab.processors.Processor
 
-class TextReader(val proc: Processor) {
+import scala.io.Source
 
-  def processText(text: String): Seq[OdinsonDocument] = {
+object TextReader{
+  def fromFile(proc: Processor, ruleFile: String): TextReader = {
+    val yml = using(Source.fromFile(ruleFile)) { ff =>
+      ff.getLines().mkString
+    }
+    new TextReader(proc, yml)
+  }
+}
+class TextReader(val proc: Processor, val rules: String) {
+
+  val config = ConfigFactory.load()
+  val numEvidenceDisplay = config.get[Int]("ui.numEvidenceDisplay").getOrElse(3)
+  val consolidateByLemma = config.get[Boolean]("ui.lemmaConsolidation").getOrElse(true)
+
+  def extractMatches(text: String): Seq[Match] = {
     val procDoc = proc.annotate(text)
-    val odinsonDocument =
+    val odinsonDocument = ProcessorsUtils.convertDocument(procDoc)
+    val ee = mkExtractorEngine(odinsonDocument)
+    val reader = new CorpusReader(ee, numEvidenceDisplay, consolidateByLemma)
+    reader.extractMatches(rules)
+  }
+
+  /**
+   * Constructs an [[ai.lum.odinson.ExtractorEngine]] from a single-doc in-memory index ([[org.apache.lucene.store.RAMDirectory]])
+   */
+  def mkExtractorEngine(doc: OdinsonDocument): ExtractorEngine = {
+    ExtractorEngine.inMemory(doc)
   }
 
 }
